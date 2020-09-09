f5553bdf2a3e9473a53459f49e3d7ea87793db56
hyun <enjoysoft@naver.com>
2020-09-09 10:25:20 +0900

    - nuxt generate 폴더 변경

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,5 +56,11 @@ export default {
   ** See https://nuxtjs.org/api/configuration-build/
   */
   build: {
-  }
+  },
+  /**
+   * Generate configuration
+   */
+  generate: {
+    dir: 'docs'
+  },
 }
