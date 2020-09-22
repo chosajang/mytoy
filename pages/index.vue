5989b59873439edd11b8ae81edff6f2bf04d124f
hyun <enjoysoft@naver.com>
2020-09-22 10:36:16 +0900

    - 호출 레이아웃 변경

--- pages/index.vue
+++ pages/index.vue
@@ -3,33 +3,15 @@
     <div>
       <Logo />
       <h1 class="title">
-        chosajang.github.io
+        -t-?
       </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  layout: 'service'
+  layout: 'Service'
 }
 </script>
 
